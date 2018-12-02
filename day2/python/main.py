3a595c54e97f6f1f54ce63a1fb8a7e57cd6fc171
Laged <parkkila.matti@gmail.com>
2018-12-02 11:42:00 +0200

    Add day2/challenge2 with python

--- day2/python/main.py
+++ day2/python/main.py
@@ -1,3 +1,5 @@
+import difflib
+
 def check_id(id):
     chars = set(id)
     counts = list(map(lambda char: id.count(char), chars))
@@ -5,16 +7,43 @@ def check_id(id):
     triple = 3 in counts
     return double, triple
 
+def match_result(id1, id2):
+    if len(id1) != len(id2):
+        return False
+    different_chars = 0
+    shared_chars = []
+    for (index, char) in enumerate(list(id1)):
+        if char != id2[index]:
+            different_chars += 1
+            if different_chars > 1:
+                return None
+        else:
+            shared_chars += char
+    if different_chars == 1:
+        return "".join(shared_chars)
+    return None
+
 def main():
     doubles = 0
     triples = 0
+    match_found = False
     with open("input.txt") as input:
-        for id in input:
+        ids = [line.strip() for line in input]
+        for id in ids:
             double, triple = check_id(id.strip())
             if (double):
                 doubles += 1
             if (triple):
                 triples += 1
+            # Challenge 2
+            if not match_found:
+                matches = difflib.get_close_matches(id, ids, 2, 0.2)
+                if matches:
+                    result = match_result(id, matches[1])
+                    if (result):
+                        print(f"Answer to challenge #2: {result}")
+                        match_found = True
+
     checksum = doubles * triples
     print(f"Answer to challenge #1: {checksum}")
 
